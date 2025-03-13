8252ddf21ecf2f174cdc9bcbf09c3958fb84e5f8
Nekszii <ezorito437@gmail.com>
2025-03-13 21:56:20 +0100

    Add files via upload

---
+++ VulnScanner.py
@@ -0,0 +1,45 @@
+import nmap
+import threading
+
+print("1. Scan\n2.ScanVuln(scans for known vulns)\n")
+print("If it prints nothing it means there is probably nothing open!\n")
+Choice = input("Enter your choice: ")
+
+
+def Scan(Target):
+    Scan = nmap.PortScanner()
+    Scan.scan(Target, arguments="-sS")
+
+    for host in Scan.all_hosts():
+        print(f"Host: {host} ({Scan[host].hostname()})")
+        print(f"State: {Scan[host].state()}")
+
+    for proto in Scan[host].all_protocols():
+            print(f"Protocol: {proto}")
+            ports = Scan[host][proto].keys()
+            for port in ports:
+             print(f"Port: {port}, State: {Scan[host][proto][port]['state']}, Service: {Scan[host][proto][port]['name']}")
+
+def ScanVuln(Target):
+    Scan = nmap.PortScanner()
+    Scan.scan(Target, arguments="-sS --script vuln")
+
+    for host in Scan.all_hosts():
+        print(f"Host: {host} ({Scan[host].hostname()})")
+        print(f"State: {Scan[host].state()}")
+
+    for proto in Scan[host].all_protocols():
+            print(f"Protocol: {proto}")
+            ports = Scan[host][proto].keys()
+            for port in ports:
+             print(f"Port: {port}, State: {Scan[host][proto][port]['state']}, Service: {Scan[host][proto][port]['name']}")
+
+
+target = "wiesgeerts.be"
+match Choice:
+    case "1":
+        Scan(target)
+    case "2":
+        ScanVuln(target)
+    
+          
